b3429de136a8f83b76f45c5dee519b79d5a2208e
jeroendedauw <jeroendedauw@gmail.com>
2015-09-05 01:28:11 +0200

    6 -> Fizz

--- src/Nyan.clj
+++ src/Nyan.clj
@@ -1,9 +1,11 @@
 (use 'clojure.test)
 
+(defn is-divisible [number devisor] (== 0 (mod number devisor)))
+
 (defn fizz-buzz [number]
-  (if (= number 3)
+  (if (is-divisible number 3)
     "Fizz"
-    (if (= number 5)
+    (if (is-divisible number 5)
       "Buzz"
       (str number)
     )
@@ -14,5 +16,6 @@
 (deftest two-is-returned-as-is (is (= "2" (fizz-buzz 2))))
 (deftest three-is-fizz (is (= "Fizz" (fizz-buzz 3))))
 (deftest five-is-buzz (is (= "Buzz" (fizz-buzz 5))))
+(deftest six-is-fizz (is (= "Fizz" (fizz-buzz 6))))
 
 (run-tests)
