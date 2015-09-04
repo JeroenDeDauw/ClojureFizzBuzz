e4bf80e958f13c7a62c6e891998f9f6146f2aa14
jeroendedauw <jeroendedauw@gmail.com>
2015-09-05 01:21:29 +0200

    Improve formatting

--- src/Nyan.clj
+++ src/Nyan.clj
@@ -1,9 +1,14 @@
 (use 'clojure.test)
 
-(defn fizz-buzz [number] (if (= number 3) "Fizz" (str number)))
+(defn fizz-buzz [number] (
+  if (= number 3)
+    "Fizz"
+    (str number)
+))
 
 (deftest one-is-returned-as-is (is (= "1" (fizz-buzz 1))))
 (deftest two-is-returned-as-is (is (= "2" (fizz-buzz 2))))
 (deftest three-is-fizz (is (= "Fizz" (fizz-buzz 3))))
+;(deftest five-is-buzz (is (= "Buzz" (fizz-buzz 5))))
 
 (run-tests)
