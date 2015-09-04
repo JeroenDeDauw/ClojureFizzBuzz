b9e61f1e6272af2c4e222c000264509a0a7f8c88
jeroendedauw <jeroendedauw@gmail.com>
2015-09-05 01:16:40 +0200

    2 -> 2

--- src/Nyan.clj
+++ src/Nyan.clj
@@ -1,7 +1,8 @@
 (use 'clojure.test)
 
-(defn fizz-buzz [number] 1)
+(defn fizz-buzz [number] number)
 
 (deftest one-is-returned-as-is (is (= 1 (fizz-buzz 1))))
+(deftest two-is-returned-as-is (is (= 2 (fizz-buzz 2))))
 
 (run-tests)
