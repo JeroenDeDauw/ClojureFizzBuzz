b8fb2a78bb634db6308cd30d4737a2a2bc4d04d3
jeroendedauw <jeroendedauw@gmail.com>
2015-09-05 01:18:34 +0200

    3 -> Fizz

--- src/Nyan.clj
+++ src/Nyan.clj
@@ -1,8 +1,9 @@
 (use 'clojure.test)
 
-(defn fizz-buzz [number] number)
+(defn fizz-buzz [number] (if (= number 3) "Fizz" number))
 
 (deftest one-is-returned-as-is (is (= 1 (fizz-buzz 1))))
 (deftest two-is-returned-as-is (is (= 2 (fizz-buzz 2))))
+(deftest three-is-fizz (is (= "Fizz" (fizz-buzz 3))))
 
 (run-tests)
